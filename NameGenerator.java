f62f0eed827811aca7dc064911481b433a4c0cc2
Cristian <asdf@correo.com>
2014-04-04 12:55:04 +0200

    metodo generateStarWarsName que devuelve tu nombre starwars tomando como parametros tunombre, tu apellido, el nombre de tu madre y tu ciudad de nacimiento

--- NameGenerator.java
+++ NameGenerator.java
@@ -13,7 +13,37 @@ public class NameGenerator
      */
     public NameGenerator()
     {
-
     }
 
+    public String generateStarWarsName(String tuNombre, String tuApellido, String mamaNombre, String ciudadNacimiento){
+    String nombreStarWars;
+    String apellidoStarWars;
+    if(tuNombre.length() > 3){
+        nombreStarWars = tuNombre.substring(0,3);
+    }
+    else {
+        nombreStarWars = tuNombre;
+    }
+        if(tuApellido.length() > 3){
+        nombreStarWars += tuApellido.substring(0,3);
+    }
+    else {
+        nombreStarWars += tuApellido;
+    }
+
+        if(mamaNombre.length() > 3){
+        apellidoStarWars = mamaNombre.substring(0,3);
+    }
+    else {
+        apellidoStarWars = mamaNombre;
+    }
+        if(ciudadNacimiento.length() > 3){
+        apellidoStarWars += ciudadNacimiento.substring(0,3);
+    }
+    else {
+        apellidoStarWars += ciudadNacimiento;
+    }
+    
+    return nombreStarWars + " " + apellidoStarWars;
+    }
 }
