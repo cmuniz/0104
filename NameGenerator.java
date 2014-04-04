a53f54c1760a3b4725881881bc912ab24d9acd17
Cristian <asdf@correo.com>
2014-04-04 12:56:19 +0200

    El metodo generateStarWarsName devuelve en nombre y apellido la primera en mayuscula y el resto en minusculas

--- NameGenerator.java
+++ NameGenerator.java
@@ -16,34 +16,39 @@ public class NameGenerator
     }
 
     public String generateStarWarsName(String tuNombre, String tuApellido, String mamaNombre, String ciudadNacimiento){
-    String nombreStarWars;
-    String apellidoStarWars;
-    if(tuNombre.length() > 3){
-        nombreStarWars = tuNombre.substring(0,3);
-    }
-    else {
-        nombreStarWars = tuNombre;
-    }
+        String nombreStarWars;
+        String apellidoStarWars;
+
+        if(tuNombre.length() > 3){
+            nombreStarWars = tuNombre.substring(0,3);
+        }
+        else {
+            nombreStarWars = tuNombre;
+        }
         if(tuApellido.length() > 3){
-        nombreStarWars += tuApellido.substring(0,3);
-    }
-    else {
-        nombreStarWars += tuApellido;
-    }
+            nombreStarWars += tuApellido.substring(0,3);
+        }
+        else {
+            nombreStarWars += tuApellido;
+        }
+
+        nombreStarWars = nombreStarWars.substring(0,1).toUpperCase() + nombreStarWars.substring(1).toLowerCase();
 
         if(mamaNombre.length() > 3){
-        apellidoStarWars = mamaNombre.substring(0,3);
-    }
-    else {
-        apellidoStarWars = mamaNombre;
-    }
+            apellidoStarWars = mamaNombre.substring(0,3);
+        }
+        else {
+            apellidoStarWars = mamaNombre;
+        }
         if(ciudadNacimiento.length() > 3){
-        apellidoStarWars += ciudadNacimiento.substring(0,3);
-    }
-    else {
-        apellidoStarWars += ciudadNacimiento;
-    }
-    
-    return nombreStarWars + " " + apellidoStarWars;
+            apellidoStarWars += ciudadNacimiento.substring(0,3);
+        }
+        else {
+            apellidoStarWars += ciudadNacimiento;
+        }
+
+        apellidoStarWars = apellidoStarWars.substring(0,1).toUpperCase() + apellidoStarWars.substring(1).toLowerCase();
+
+        return nombreStarWars + " " + apellidoStarWars;
     }
 }
