dc7f5f34f5f6737c2348309221f969ee232092e1
Cristian <asdf@correo.com>
2014-04-04 12:03:43 +0200

    Creada clase NameGenerator

---
+++ NameGenerator.java
@@ -0,0 +1,19 @@
+
+/**
+ * Write a description of class NameGenerator here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class NameGenerator
+{
+
+    /**
+     * Constructor for objects of class NameGenerator
+     */
+    public NameGenerator()
+    {
+
+    }
+
+}
